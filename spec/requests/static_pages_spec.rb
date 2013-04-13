2a313de41a7e26df1a684e6b10bde6f0b33e5ae6
Jeff <jstandard@gmail.com>
2013-04-13 14:49:18 -0500

    layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,38 +2,31 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => "Sample App")
-    end
+  let (:base_title) {"yosup |"}
+  subject{page}
 
-    it "should have the right title" do
-  		visit '/static_pages/home'
-  		page.should have_selector('title', :text => "yosup | Home")
-  	end
+  describe "Home page" do
+    before {visit root_path}
+    it  {should have_selector('h1', text: 'Saitama Kiwi homepage')}
+    it  {should have_selector('title', text: "yosup")}
+    it  {should_not have_selector('title', text: '| Home')}
   end
 
   describe "Help page" do
-  	it "should have the content 'help'" do
-  		visit '/static_pages/help'
-  		page.should have_selector('h1', :text => "help")
-  	end
-
-  	it "should have the right title" do
-  		visit '/static_pages/help'
-  		page.should have_selector('title', :text => "yosup | Help")
-  	end
+  	before {visit help_path}
+    it  {should have_selector('h1', text: 'help')}
+    it  {should have_selector('title', text: "#{base_title} Help")}
   end
 
   describe "about page" do
-  	it "should have the content 'about'" do
-  		visit '/static_pages/about'
-  		page.should have_selector('h1', :text => "about us")
-  	end
-  	it "should have the right title" do
-  		visit '/static_pages/about'
-  		page.should have_selector('title', :text => "yosup | About")
-  	end
+    before {visit about_path}  	
+    it  {should have_selector('h1', text: 'about us')}
+    it  {should have_selector('title', text: "#{base_title} About")}
+  end
+
+  describe "contact page" do
+    before {visit contact_path}   
+    it  {should have_selector('h1', text: 'contact us')}
+    it  {should have_selector('title', text: "#{base_title} Contact")}
   end
 end
