2ce4b0cee2b7e5263576936b3a56831e958b1d5e
Jeff <jstandard@gmail.com>
2013-04-20 18:17:44 -0500

    misc 7.6 exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,7 +12,12 @@ describe "UserPages" do
 		it {should have_selector('h1', text:'Sign up')}
 		it {should have_selector('title', text: "#{base_title} Sign Up")}
 
-		describe "blank submission should not create user" do
+		describe "with invalid information" do
+			describe "after submission" do
+				before {click_button submit}
+				it {should have_selector('title', text: 'Sign Up')}
+				it {should have_content('error')}
+			end
 			it "should not create a user" do
 				expect {click_button submit}.not_to change(User, :count)
 			end
@@ -27,7 +32,13 @@ describe "UserPages" do
 			end
 			it "should create a new user" do
 				expect {click_button submit}.to change(User, :count).by(1)
-			end				
+			end
+			# describe "after saving the user" do
+			# 	before {click_button submit}
+			# 	let(:user){User.find_by_email('user@example.com')}
+			# 	it {should have_selector('title', text: user.name)}
+			# 	it {should have_selector('div.alert.alert-success', text: 'Welcome')}
+			# end
 		end
 	end
 
